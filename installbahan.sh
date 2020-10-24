432dce38cb3be32361dbb79ce4be9920263da5e0
mrzomby <73274507+mrzomby@users.noreply.github.com>
2020-10-24 13:32:46 +0700

    Add files via upload

---
+++ installbahan.sh
@@ -0,0 +1,3 @@
+z="
+";Gz=' Bah';Pz='grad';hz=' mec';xz='AN"';Oz='t up';bz='igle';Lz='upda';Xz='on p';rz='IL M';fz='pip2';Hz='an-B';Cz='dang';Iz='ahan';mz=' req';Zz='n2 v';uz='LL B';Vz='pkg ';Mz='te &';gz=' ins';az='im f';Yz='ytho';sz='ENGI';oz='s';Dz=' Men';Nz='& ap';Sz='all ';lz='cat';Jz='"';jz='ze';Az='echo';Kz='apt ';Uz='git';Rz='inst';wz='-BAH';nz='uest';vz='AHAN';kz=' lol';iz='hani';tz='NSTA';Ez='gins';ez='php';Bz=' "Se';qz='RHAS';Fz='tall';pz=' "BE';dz='rl';Qz='e';Tz='nano';Wz='pyth';cz='t cu';
+eval "$Az$Bz$Cz$Dz$Ez$Fz$Gz$Hz$Iz$Jz$z$Kz$Lz$Mz$Nz$Oz$Pz$Qz$z$Kz$Rz$Sz$Tz$z$Kz$Rz$Sz$Uz$z$Vz$Rz$Sz$Wz$Xz$Yz$Zz$az$bz$cz$dz$z$Kz$Rz$Sz$ez$z$fz$gz$Fz$hz$iz$jz$z$fz$gz$Fz$kz$lz$z$fz$gz$Fz$mz$nz$oz$z$Az$pz$qz$rz$sz$tz$uz$vz$wz$xz"
